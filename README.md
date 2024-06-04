c331c4563dae4c8374279d1d36b59adbcbec329b
Gabriella Andrade <gabii.marcelly@gmail.com>
2024-06-04 11:20:31 +0100

    step 7 handle errors

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ My shopping list project
 step 5 - clean up the list and don't duplicate items
 
 step 6 - remove items from de list when checked
+
+step 7 - handle errors 
--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,21 @@ let shoppingListArray = [];
 
 function getItem(event) {
   event.preventDefault();
-  const item = document.getElementById('item').value.trim();
-  if (item && !shoppingListArray.includes(item)) {
-    shoppingListArray.push(item);
-    shoppingListArray.sort();
-    removeList(); // limpa a lista antiga
-    addItemToList(); // adiciona a nova lista ordenada
-    document.getElementById('myForm').reset(); // limpa o input
-  }
+    const item = document.getElementById('item').value.trim();
+    try {
+      if (!item) {
+        throw new Error('Item cannot be empty');
+      }
+      if (item && !shoppingListArray.includes(item)) {
+        shoppingListArray.push(item);
+        shoppingListArray.sort();
+        removeList(); // limpa a lista antiga
+        addItemToList(); // adiciona a nova lista ordenada
+        document.getElementById('myForm').reset(); // limpa o input
+      }  
+    } catch (error) {
+      console.error(error); // trata o erro conforme o necessário
+    }
   console.log(shoppingListArray);
 }
 
@@ -32,12 +39,17 @@ function addItemToList() {
 
 function removeItem(event) {
   const item = event.currentTarget.textContent;
-  const index =shoppingListArray.indexOf(item);
+  try {
+    const index =shoppingListArray.indexOf(item);
   if (index > -1) {
+    throw new Error('Item not found in the list');
+    }
     shoppingListArray.splice(index, 1);
     removeList(); //limpa a lista antiga
     addItemToList(); //adiciona nova lista
-  } 
+  } catch (error) {
+    console.error(error); // trata o erro conforme necessário
+  }
 }
 
 document.getElementById('shoppingList').addEventListener('click', function(event) {
